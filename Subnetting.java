6b492a233691333598b0bdc8caab1dafd5efb60e
Omkar Mankar <mankar.omkar2310@gmail.com>
2019-10-25 12:01:31 +0530

    updated subnet practical

--- Subnetting.java
+++ Subnetting.java
@@ -5,168 +5,163 @@ for a network 192.168.4.10 / 26 (form 4 subnets) and find the sub net masks.
 import java.util.Scanner;
 import java.net.InetAddress;
 
-class subnet
-{
+import java.util.*;
+import java.io.*;
+public class SUB {
 
-public static void main(String args[]) 
-{
-	
-Scanner sc= new Scanner(System.in);
-System.out.print("Enter the ip address=");
-String ip=sc.nextLine();
+	public static void main(String[] args) {
+	Scanner s1=new Scanner(System.in);
+		String ipadd="";
+		System.out.println("ENter IP Address");
+		
+		ipadd=s1.nextLine();
+		
+		String splitip[]=ipadd.split("\\.");
+		
+		//convert binary
+		String binip="";
+		for(int i=0;i<4;i++)
+		{
+			splitip[i]=appendZeros(Integer.toBinaryString(Integer.parseInt(splitip[i])));
+			binip+=splitip[i];
+		}
+		
+		System.out.println("Binary is : " + binip);
+		
+		System.out.println("ENter CIDR bits");
+		int cidr=s1.nextInt();
+		int bits=8-(cidr%8);
+		int total_address=(int)Math.pow(2,bits);
+		getsubnet(cidr);
+		
+		//convert binary
+		int firstaddbint[]=new int[32];
+		int lastaddbint[]=new int[32];
+		
+		for(int i=0;i<32;i++)
+		{
+			firstaddbint[i]=binip.charAt(i) - 48;
+			lastaddbint[i]=binip.charAt(i) - 48;
+		}
+		
+		//anding
 
 
-//----Split the Ip Address-------
-
-String split_ip[] = ip.split("\\.");
+		
+		for(int i=31;i>31-bits;i--)
+		{
+			firstaddbint[i]=firstaddbint[i] & 0;
+		}
 		
 
-//----- Converting the Ip Address to binary----
+		
+		//move this to a string array
+		String ipinBin[]= {"","","",""};
+		for(int i=0;i<32;i++)
+		{
+			ipinBin[i/8]=new String(ipinBin[i/8]+firstaddbint[i]);
+		}
+		
+		//convert to a decimal value
+		int FirstAddGrp[]=new int[4];
+		for(int i=0;i<4;i++)
+		{
+			FirstAddGrp[i]=Integer.parseInt(ipinBin[i],2);
+			System.out.print(FirstAddGrp[i]);
+			if(i!=3)
+			{
+				System.out.print(".");
+			}
+		}
+		
+		
+		//get last
+		
+		for(int i=31;i>31-bits;i--)
+		{
+			lastaddbint[i]=lastaddbint[i] | 1;
+		}
+		
+		//move this to a string
+		
+		String LastipGrp[]= {"","","",""};
+		for(int i=0;i<32;i++)
+		{
+			LastipGrp[i/8]=new String (LastipGrp[i/8]+lastaddbint[i]);
+		}
+		
+		//cnvrt in decimal
+		int LastipGrpinDec[]=new int[4];
+		for(int i=0;i<4;i++)
+		{
+			LastipGrpinDec[i]=Integer.parseInt(LastipGrp[i],2);
+			System.out.print(LastipGrpinDec[i]);
+			if(i!=3)
+			{
+				System.out.print(".");
+			}
+		}
+		
+		System.out.println("How many subnets you want to form");
+		int scont=s1.nextInt();
+		for(int j=1;j<scont;j++)
+		{
+			System.out.println("Group " + (j+1)+ "First Address : ");
+			
+			for(int i=0;i<4;i++)
+			{
+				if(i<3)
+				{
+				System.out.print(FirstAddGrp[i] + " .");
+				}
+				else
+				{
+					System.out.print(FirstAddGrp[i]=FirstAddGrp[i]+total_address);
+				}
+			}
+			System.out.println("Group " + (j+1)+ "Last Address : ");
+			
+			for(int i=0;i<4;i++)
+			{
+				if(i<3)
+				{
+				System.out.print(LastipGrpinDec[i] + " .");
+				}
+				else
+				{
+					System.out.print(LastipGrpinDec[i]=LastipGrpinDec[i]+total_address);
+				}
+			}
+		}
+		
+		
+	}
 
-String split_bip[]= new String[4];
+	private static void getsubnet(int cidr) {
+		// TODO Auto-generated method stub
+		
+		int x=cidr%8;
+		int t=8-x;
+		int a=1;
+		int lastbit=0;
+		while(a<=x)
+		{
+			lastbit+=Math.pow(2,t);
+			t++;
+			a++;
+		}
+		String subnemask="255.255.255.";
+		
+		subnemask+=String.valueOf(lastbit);
+		
+		System.out.println("SUbnet is : " + subnemask);
+	}
 
-String bip = "";
-
-for(int i=0;i<4;i++)
-{
-split_bip[i]=appendZeroes(Integer.toBinaryString(Integer.parseInt(split_ip[i])));
-bip+=split_bip[i];
+	private static String appendZeros(String binaryString) {
+		// TODO Auto-generated method stub
+		String temp="00000000";
+		return temp.substring(binaryString.length()) + binaryString;
+	}	
+	
 }
-System.out.println("The binary IpAddress is="+bip);
-
-//----- Finding the Subent mask-----
-
-System.out.println("Enter the number of address");
-int n=sc.nextInt();
-
-int bits=(int)Math.ceil(Math.log(n)/Math.log(2));
-System.out.println("The number of bits required="+bits);
-
-int mask=32-bits;
-int total_address=(int)Math.pow(2,bits);
-System.out.println("Subnet mask is "+mask);
-
-//---- Finding the first and last address----
-
-//---- First address Calculation--------
-int fbip[]=new int[32];
-
-for(int i=0;i<32;i++)
-{
-//Convert to the character 1,0 to integer 1,0
-
-fbip[i]=(int)bip.charAt(i)-48;
-
-}
-
-for(int i=31;i>31-bits;i--)
-{
-//Get first address by anding the last bits with 0
-
-fbip[i] &=0;
-}
-
-String fip[]={"","","",""};
-for(int i=0;i<32;i++)
-{
-fip[i/8]=new String(fip[i/8]+fbip[i]);
-} 
-int first_offset=0;
-int ipAddr[]=new int[4];  	;
-System.out.println("Group 1 \nThe First Address is:");
-for(int i=0;i<4;i++)
-{
-System.out.print(ipAddr[i]=first_offset=Integer.parseInt(fip[i],2));
-if(i!=3)
-System.out.print(".");
-}
-System.out.println();
-
-
-//--- Last address Calculation----
-int lbip[]=new int [32];
-
-for(int i=0;i<32;i++)
-{
-// Convert the character 1,0 to integer 1,0
-
-lbip[i]=(int)bip.charAt(i)-48;
-}
-
-for(int i=31;i>31-bits;i--)
-{
-
-// Get last address by oring with last bits with 1
-
-lbip[i]|= 1;
-}
- String lip[]={"","","",""};
-for(int i=0;i<32;i++)
-{
-lip[i/8]=new String(lip[i/8]+lbip[i]);
-}
-int ipLast[]=new int[4]; 
-System.out.println("The Last Address is:");
-for(int i=0;i<4;i++)
-{
-System.out.print(ipLast[i]=Integer.parseInt(lip[i],2));
-if(i!=3)
-System.out.print(".");
-}
-System.out.println();
-System.out.println("How many subnets do you want to form?");
-int scount=sc.nextInt();
-for(int j=1;j<scount;j++)
-{
-System.out.println(" GROUP "+ (j+1)+" FIRST ADDRESS:");
-for(int i=0;i<4;i++)
-{
-if(i<3)
-{
-System.out.print(ipAddr[i]+".");
-}
-else
-System.out.println(ipAddr[i]=ipAddr[i]+total_address);
-}
-System.out.println(" GROUP "+ (j+1)+" LAST ADDRESS:");
-for(int i=0;i<4;i++)
-{
-if(i<3)
-{
-System.out.print(ipLast[i]+".");
-}
-else
-System.out.println(ipLast[i]=ipLast[i]+total_address);
-
-}
-System.out.println();
-}
-try
-{
-
-System.out.println("Enter the Ip address to ping=");
-Scanner s=new Scanner(System.in);
-String ip_add=s.nextLine();
- InetAddress inet = InetAddress.getByName(ip_add);
-if(inet.isReachable(5000))
-{
-System.out.println("The ip address is reachable"+ip_add);
-}
-else
-{
-System.out.println("The ip address is not reachable"+ip_add);
-}
-}
- catch( Exception e)
-{
-System.out.println("Exception:"+e.getMessage());
-}
-}
-
-static String appendZeroes(String s)
-{
-String temp= new  String("00000000");
-return temp.substring(s.length())+ s;
-}
-} 
 
